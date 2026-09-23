b63f34a6a4203f8f2a53778b43a9a636d901a208
MaxiGP0 <maximogomezpessoa@gmail.com>
2026-09-23 17:49:46 -0300

    miniavance

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
+
+#define FILEDIRECTORY ""
 
 
 #define MEMSIZE 4096
@@ -7,30 +10,49 @@
 #define VxSIZE 16
 
 
+/**
+ *  Estructura de datos para representar el estado del procesador.
+ *  La memoria, la pila y los registros.
+ */
+
 typedef struct machine_t {
 
-    uint8_t mem[MEMSIZE];
-    uint16_t pc;
+    uint8_t mem[MEMSIZE];               // Memoria disponible para la CPU.
+    uint16_t pc;                        // Contador de programa.            
 
-    uint16_t stack[STACKSIZE];
-    uint16_t sp;
+    uint16_t stack[STACKSIZE];          // Pila. 16 registros de 16 bits.
+    uint16_t sp;                        // Puntero de pila.
 
-    uint8_t v[VxSIZE];
-    uint16_t i;
-    uint8_t dt, st;
+    uint8_t v[VxSIZE];                  // 16 registros generales.
+    uint16_t i;                         // Registro especial.
+    uint8_t dt, st;                     // Temporizadores.
 
 } machine_t;
+
+
+
+void load_rom(machine_t machine) {
+
+    FILE* fp = fopen(FILEDIRECTORY, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Cannot open ROM file.\n");
+        exit(1);
+    }
+    
+    
+    fclose(fp);
+
+}
 
 
 
 
 int main(int argc, char** argv) {
     
-    
-    
-    
+    machine_t mac;
+    load_rom(&mac);
 
-    printf("a\n");
+
     return 0;
 
 }
